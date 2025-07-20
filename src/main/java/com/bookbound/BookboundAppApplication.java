1221089a4e0cbed58e128e93b1924b2abfcf0aaa
Denis Kalala <kalaladm@mail.uc.edu>
2025-07-20 17:56:48 -0400

    chore(UI): add layout.html, index.html, style.css and basic UI structure

---
+++ src/main/java/com/bookbound/BookboundAppApplication.java
@@ -0,0 +1,13 @@
+package com.bookbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookboundAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookboundAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookbound/BookboundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookboundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
