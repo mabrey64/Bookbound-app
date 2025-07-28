55c121b3572e156acce32d8adbfd4ad54221008f
Denis Kalala <kalaladm@mail.uc.edu>
2025-07-27 20:40:22 -0400

    Added the UI for the begging of the application.

---
+++ src/main/java/com/bookbound/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.bookbound.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String landing() {
+        return "landing";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
